c8b4a8cb4c0520d47a23cfb9de72c0e6355308a1
Tredicibrando <nicolosigiov@gmail.com>
2022-03-15 14:35:44 +0100

    Struttura

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,34 @@
+<template>
+    <header class="header">
+        <figure>
+            <img src="../assets/Netflix-Logo.svg" alt="">
+        </figure>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'MyHeader',
+    }
+</script>
+
+<style lang="scss" scoped>
+
+$black: #161616;
+
+    .header{
+        display: flex;
+        height: 70px;
+        background-color: $black;
+        align-items: center;
+
+        figure{
+            width: max-content;
+            padding: 10px;
+
+            img{
+                height: 40px;
+            }
+        }
+    }
+</style>
